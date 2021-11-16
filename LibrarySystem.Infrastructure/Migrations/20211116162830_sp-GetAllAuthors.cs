54eb81537f248102c0e29b5f73e507c53368fd49
SteveSB <mustafa.baghdadi.1996@hotmail.com>
2021-11-16 18:47:07 +0200

    [add] Author and Category StoredProcedures

---
+++ LibrarySystem.Infrastructure/Migrations/20211116162830_sp-GetAllAuthors.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibrarySystem.Infrastructure.Migrations
+{
+    public partial class spGetAllAuthors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[GetAllAuthors]
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+                    select * from Authors
+                END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ LibrarySystem.Infrastructure/Migrations/20211116163001_sp-GetAllCategories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibrarySystem.Infrastructure.Migrations
+{
+    public partial class spGetAllCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[GetAllCategories]
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+                    select Id, Name from Categories
+                END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
