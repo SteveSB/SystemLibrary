2f8ebe43f0bcb4fe1b3e04b9c5653687b936ac44
SteveSB <mustafa.baghdadi.1996@hotmail.com>
2021-11-16 23:15:27 +0200

    [change] disable cascade delete behavior in DB

--- LibrarySystem.Infrastructure/ApplicationDbContext.cs
+++ LibrarySystem.Infrastructure/ApplicationDbContext.cs
@@ -15,6 +15,16 @@ namespace LibrarySystem.Infrastructure
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
---
+++ LibrarySystem.Infrastructure/Migrations/20211116211309_disable-cascading.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibrarySystem.Infrastructure.Migrations
+{
+    public partial class disablecascading : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_SubCategories_SubCategoryId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_SubCategories_SubCategoryId",
+                table: "Books",
+                column: "SubCategoryId",
+                principalTable: "SubCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_SubCategories_SubCategoryId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_SubCategories_SubCategoryId",
+                table: "Books",
+                column: "SubCategoryId",
+                principalTable: "SubCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
